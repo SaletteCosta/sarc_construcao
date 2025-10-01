aafc139b698824f2baf9edb032c63e75a3ce8835
Salette Cristina da Costa <sccosta@ps.grupopanvel.com.br>
2025-10-01 18:42:07 -0300

    remove notifications'

--- src/main/java/com/sarc/repository/NotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sarc.repository;
-
-import com.sarc.domain.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {}
